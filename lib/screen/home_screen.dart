de8ae05e5a6706a68e1c55d46e33552b1bcfa776
hommehyuk <bin6827@gmail.com>
2024-03-18 17:30:22 +0900

    ListView 위젯 사용

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -1,6 +1,7 @@
 import 'package:calendar_scheduler/component/calendar.dart';
 import 'package:calendar_scheduler/component/schedule_card.dart';
 import 'package:calendar_scheduler/component/today_banner.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -35,13 +36,23 @@ class _HomeScreenState extends State<HomeScreen> {
               scheduleCount: 3,
             ),
             SizedBox(height: 8.0),
-            Padding(
-              padding: EdgeInsets.symmetric(horizontal: 8.0),
-              child: ScheduleCard(
-                startTime: 8,
-                endTime: 9,
-                content: '프로그래밍 공부하기.',
-                color: Colors.red,
+            Expanded(
+              child: Padding(
+                padding: EdgeInsets.symmetric(horizontal: 8.0),
+                child: ListView.separated(
+                  itemCount: 100,
+                  separatorBuilder: (context, index) {
+                    return SizedBox(height: 8.0);
+                  },
+                  itemBuilder: (context, index) {
+                    return ScheduleCard(
+                      startTime: 8,
+                      endTime: 9,
+                      content: '프로그래밍 공부하기. $index',
+                      color: Colors.red,
+                    );
+                  },
+                ),
               ),
             ),
           ],
