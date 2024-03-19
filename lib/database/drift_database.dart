8358c52603f50183f151b42a70648950f63748a0
hommehyuk <bin6827@gmail.com>
2024-03-19 15:39:26 +0900

    Flutter의 특별한 기능 - Code Generation

--- lib/database/drift_database.dart
+++ lib/database/drift_database.dart
@@ -18,6 +18,18 @@ part 'drift_database.g.dart';
 )
 class LocalDatabase extends _$LocalDatabase {
   LocalDatabase() : super(_openConnection());
+
+  Future<int> createSchedule(SchedulesCompanion data) =>
+      into(schedules).insert(data);
+
+  Future<int> createCategoryColor(CategoryColorsCompanion data) =>
+      into(categoryColors).insert(data);
+
+  Future<List<CategoryColor>> getCategoryColors() =>
+      select(categoryColors).get();
+
+  @override
+  int get schemaVersion => 1;
 }
 
 LazyDatabase _openConnection() {
