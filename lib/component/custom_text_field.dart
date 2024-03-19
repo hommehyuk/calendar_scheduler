372b375546d6bd4c6666fe8236e40470a8a0e756
hommehyuk <bin6827@gmail.com>
2024-03-19 16:21:26 +0900

    ValidationMode 사용

--- lib/component/custom_text_field.dart
+++ lib/component/custom_text_field.dart
@@ -43,6 +43,21 @@ class CustomTextField extends StatelessWidget {
         if (val == null || val.isEmpty) {
           return '값을 입력 해주세요.';
         }
+
+        if (isTime) {
+          int time = int.parse(val);
+
+          if (time < 0) {
+            return '0 이상의 숫자를 입력 해주세요.';
+          }
+          if (time > 24) {
+            return '24 이하의 숫자를 입력 해주세요.';
+          }
+        } else {
+          if (val.length > 500) {
+            return '500자 이하의 글자를 입력 해주세요.';
+          }
+        }
         return null;
       },
       cursorColor: Colors.grey,
